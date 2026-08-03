7a017600ada16884f4e8337bbdcfe02c02658d9a
Md Asharaf <718romanempire@gmail.com>
2026-08-04 03:12:01 +0530

    enable hoverOnlyWhenSupported in tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
