9db845d8a2e31b1ca2933719f053cf1a3716bdfb
Md Asharaf <718romanempire@gmail.com>
2026-08-04 03:30:01 +0530

    change resume card title alignment from center to start

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -56,7 +56,7 @@ export const ResumeCard = ({
         <CardHeader>
           <div className="flex items-center justify-between gap-x-2 text-base">
             <div>
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+              <h3 className="inline-flex items-start font-semibold leading-none text-xs sm:text-sm">
                 <Link href={href} className="hover:underline" onClick={(e) => e.stopPropagation()} target="_blank" rel="noopener noreferrer">
                   {title}
                 </Link>
